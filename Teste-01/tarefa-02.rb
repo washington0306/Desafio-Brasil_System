6efc7db29a7b2fe14064c775a2ae28b022a530d1
jose <josewgomes0800@gmail.com>
2024-03-22 15:39:28 -0300

    tarefa-2

---
+++ Teste-01/tarefa-02.rb
@@ -0,0 +1,7 @@
+# Tarefa - 2: Escreva um algoritmo que aceita uma string e retorne os caracteres na ordem inversa.
+
+puts 'Digite uma string: '
+string = gets.chomp
+new_string = string.strip
+result = new_string.reverse
+puts result
