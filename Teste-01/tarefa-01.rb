b341c942de10c156baf8a5b31a281236c4e9e808
jose <josewgomes0800@gmail.com>
2024-03-22 15:38:28 -0300

    tarefa-1

---
+++ Teste-01/tarefa-01.rb
@@ -0,0 +1,7 @@
+# Crie um algoritmo que receba o nome como entrada e nós dê como saída o nome 
+#digitado e o comprimento do mesmo.
+
+
+palavra = gets.chomp
+
+puts "#{palavra} #{palavra.length} caracteres"
